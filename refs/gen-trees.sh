f542b4754a7f3e5083d4a6bad91d771e29b4a9ae
Kent Watsen <kent@watsen.net>
2017-03-04 19:54:55 -0500

    this is -01

--- refs/gen-trees.sh
+++ refs/gen-trees.sh
@@ -1,8 +1,3 @@
 
-pyang -p ../ -f tree --tree-print-groupings ../ietf-tls-client\@*.yang > ietf-tls-client-tree.txt.tmp
-pyang -p ../ -f tree --tree-print-groupings ../ietf-tls-server\@*.yang > ietf-tls-server-tree.txt.tmp
-
-fold -w 71 ietf-tls-client-tree.txt.tmp > ietf-tls-client-tree.txt
-fold -w 71 ietf-tls-server-tree.txt.tmp > ietf-tls-server-tree.txt
-
-rm *-tree.txt.tmp
+pyang -p ../ -f tree --tree-line-length 71 --tree-print-groupings ../ietf-tls-client\@*.yang > ietf-tls-client-tree.txt
+pyang -p ../ -f tree --tree-line-length 71 --tree-print-groupings ../ietf-tls-server\@*.yang > ietf-tls-server-tree.txt
